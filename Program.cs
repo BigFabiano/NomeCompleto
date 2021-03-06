d56a882ea76267defcdd02d768fb07b9a6358f56
FabianoEtec <nbscbs@gmail.com>
2021-03-05 22:29:28 -0300

    Finaliza o Projeto nomeCompleto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string nome;
+           string sobrenome;
+           string nomeCompleto;
+           string nomeCatalogo;
+                      
+           Console.Write("Digite seu primeiro nome: ");
+           nome = Console.ReadLine();
+           Console.Write("Digite seu sobrenome: ");
+           sobrenome = Console.ReadLine();
+           nomeCompleto = $"{nome} {sobrenome}";
+           nomeCatalogo = $"{sobrenome.ToUpper} {nome}";
+           Console.WriteLine($"Nome completo: {nomeCompleto}");
+           Console.WriteLine($"Nome catalogo: {nomeCatalogo}");
+           
+        }
+    }
+}
